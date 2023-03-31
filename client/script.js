e70b024e1bf081f1b324efc411d913335676a867
Logan.exe <logansmith.exe@gmail.com>
2023-03-31 05:19:19 -0500

    updating server url, one more time

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv);  
 
-    const response = await fetch('https://codex-fgg2.onrender.com/', {
+    const response = await fetch('https://codex-qqqv.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
